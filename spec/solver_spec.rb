591025fb2e69cf796c3c5c1ee0766f1465b25b9b
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-01-06 15:16:10 +0300

    Add test cases for reverse string method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -19,4 +19,18 @@ describe Solver do
         it 'should return 120 when given 5' do
             expect(solver.factorial(5)).to eq(120)
         end
+    end
+
+    context 'when using reserve method' do
+        it 'should return olleh when given hello' do
+            expect(solver.reserve('hello')).to eq('olleh')
+        end
+
+        it 'should return 12345 when given 54321' do
+            expect(solver.reserve('54321')).to eq('12345')
+        end
+
+        it 'should return esiuol when given louise' do
+            expect(solver.reserve('louise')).to eq('esiuol')
+        end
 end
